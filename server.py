dfaa4c76f85bff8fca201bba5367f9d1147fdf73
bree parker <breannaparker1991@gmail.com>
2022-10-24 13:07:06 -0700

    more!!!

--- server.py
+++ server.py
@@ -4,41 +4,49 @@
 #Author Breanna Parker
 #10/24/22
 
-
+from fileinput import close
 import string
+import operator
 
 gba_dict = dict()
 
 def add_word(words,gba_dict):
   for word in words:
-    if word not in gba_dict:
-      gba_dict[word] =1
-    else:
-      gba_dict[word] += 1
+    gba_dict[word] = gba_dict.get(word,0) + 1
 
 def process_lines(line, gba_dict):
-  for line in gba_dict:
+  # for line in gba_dict:
     line = line.rstrip()
-    line = line.translate(line.maketrans('','', gba_dict.punctuation))
+    line = line.translate(line.maketrans('','', string.punctuation))
     line = line.lower()
     words = line.split()
     add_word(words,gba_dict)
   
 
 def pretty_print(gba_dict):
-  for word in sorted(gba_dict.items(), key = lambda x: (-x[1], x[0] )):
-    print (word[0], word[1])
+    for word in sorted(gba_dict, key = gba_dict.get, reverse=True):
+      print (word, gba_dict[word])
+
+def process_file(gba_dict):
+  fname = input("enter the file name:")
+  try:
+    with open (fname, 'w') as f:
+      fname.write(gba_dict)
+  except:
+    print("File cannot be opened:")
+    exit()
+
 
 def main():
   try:
-    gba_file = open("Gettysburg.txt", 'r')
+    with open("Gettysburg.txt", 'r') as gba_file:
+      for line in gba_file:
+        process_lines(line, gba_dict)
+      pretty_print(gba_dict)
   except:
     print("File cannot be opened")
     exit()
-  for line in gba_file:
-    process_lines(line, gba_dict)
-    pretty_print(gba_dict)
-  
+  process_file(gba_dict)
 if __name__ == '__main__':
   main()
   
