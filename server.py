765a6a3d0207dc8b59eb62e773149dd46738fd8f
bree parker <breannaparker1991@gmail.com>
2022-10-23 18:48:20 -0700

    first function

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
 import json
 
-with open('Gettysburg.txt') as f:
-  data = json.load(f)
+gba_file = open('Gettysburg.txt', 'r')
+gba_dict = {}
 
-for word in data:
-  print(word)
+def add_word():
+  for word in gba_file:
+    gba_dict.append(word)
+  print(gba_dict)
+
+add_word()
+
+def main(): 
+  for line in gba_file:
+    pass
+
+
+# with open('Gettysburg.txt') as f:
+#   data = json.load(f)
+
+# for word in data:
+#   print(word)
