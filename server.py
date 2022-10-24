cd4ad2750a865e185115c0d7d62feb83d823df10
bree parker <breannaparker1991@gmail.com>
2022-10-23 18:51:41 -0700

    update first function

--- server.py
+++ server.py
@@ -5,7 +5,10 @@ gba_dict = {}
 
 def add_word():
   for word in gba_file:
-    gba_dict.append(word)
+    if word in gba_dict:
+      gba_dict[word] +=1
+    else:
+      gba_dict[word] = 1
   print(gba_dict)
 
 add_word()
