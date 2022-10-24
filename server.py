b092d66df1b5e4d86bc06e9b46e651d6ed952bd2
bree parker <breannaparker1991@gmail.com>
2022-10-23 20:33:01 -0700

    correct code??

--- server.py
+++ server.py
@@ -1,28 +1,34 @@
 import json
 
 def process_line(line,word_count_dict):
-  pass
+  for line in word_count_dict:
+    line = line.rstrip()
+    line = line.translate(line.maketrans('','', word_count_dict.punctuation))
+    line = line.lower()
+    words = line.split()
+    add_word(words,word_count_dict)
 
 def add_word(word, word_count_dict):
-  for word in data:
-    if word in gba_dict:
-      gba_dict[word] +=1
+  for word in word_count_dict:
+    if word in word_count_dict:
+      word_count_dict[word] +=1
     else:
-      gba_dict[word] = 1
+      word_count_dict[word] = 1
       
       
 def pretty_print(word_count_dict):
-  print('{:11}{:11}'.format('Word', "Count"))
+  for key,value in word_count_dict():
+    print('{:11}{:11}'.format('Word', "Count"))
         
 def main():
   word_count_dict = {}
-  try:
-    with open("Gettysburg.txt", 'r') as file:
-      #####
-  except FileNotFoundError as e:
-    print(e)
-  else:
+  gba_file = open("Gettysburg.txt")
+  for line in gba_file:
+    process_line(line,word_count_dict)
+
     pretty_print(word_count_dict)
 
 if __name__ == "__main__":
   main()
+  
+main()
