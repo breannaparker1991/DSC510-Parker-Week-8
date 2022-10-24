d185133175b8fc061858290e3269c56a45a0f3df
bree parker <breannaparker1991@gmail.com>
2022-10-23 19:27:48 -0700

    next attempt

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
-from audioop import add
 import json
 
-# gba_file = open('Gettysburg.txt', 'r')
-gba_dict = {}
+gba_file = open('Gettysburg.txt')
+gba_dict = dict()
+# def add_word():
+#   with open("Gettysburg.txt") as f:
+#     for line in f:
+#       (key,val) = line.split()
+#       gba_dict[int(key)] = val
+#   print(gba_dict)
+
+
 def add_word():
-  with open("Gettysburg.txt") as f:
-    for line in f:
-      (key,val) = line.split()
-      gba_dict[int(key)] = val
+  for line in gba_file:
+    word = line.split()
+    for w in word:
+      if word not in gba_dict:
+        gba_dict[word] = 1
+      else:
+        gba_dict[word] += 1
   print(gba_dict)
-
+  
 add_word()
-
 
 # def add_word():
 #   for word in gba_file:
@@ -21,12 +30,15 @@ add_word()
 #       gba_dict[word] = 1
 #   print(gba_dict)
 
-def process_line():
-  
 
-def main(): 
-  for line in gba_file:
-    pass
+# def process_line():
+#   add_word()
+#   for d in gba_dict:
+#     d[] = d.replace
+
+# def main(): 
+#   for line in gba_file:
+#     pass
 
 
 # with open('Gettysburg.txt') as f:
